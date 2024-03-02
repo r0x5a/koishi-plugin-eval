c72e8453e58d8bb3979c1809eba5b2609ff152c7
tiger0132 <29677906+tiger0132@users.noreply.github.com>
2024-03-02 11:29:20 +0800

    Update build script

--- build/esbuild.ts
+++ build/esbuild.ts
@@ -1,8 +1,7 @@
 import { addHook } from 'yakumo'
-import {} from 'yakumo-esbuild'
+import { } from 'yakumo-esbuild'
 
 addHook('esbuild.before', (options, meta) => {
-  if (Object.keys(options.entryPoints)[0] === 'worker/internal') options.target = 'esnext';
   if (meta.name !== '@r0x5a/koishi-plugin-eval') return
   if (options.entryPoints['worker/internal']) {
     options.banner = { js: '(function (host, module, GLOBAL) {' }
